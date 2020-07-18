56f63b8fc1b605e86177dd78e48bcb7ba5d56765
Moonsur <monsur.cs.it@gmail.com>
2020-07-18 21:41:53 +0600

    model updated

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Genre(models.Model):
+    genre = models.CharField(max_length=255, null=True)
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=255, null=True)
+    release_year = models.IntegerField(null=True)
+    rate = models.FloatField(null=True)
+    number_in_stock = models.IntegerField(null=True)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now)
