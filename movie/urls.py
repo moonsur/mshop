405ed45fbb7988825c7eee8e8f0f0eb31c3832e8
Moonsur <monsur.cs.it@gmail.com>
2020-07-19 23:37:47 +0600

    complete b#14

--- movie/urls.py
+++ movie/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'movie'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:mov_id>', views.detail, name='detail')
 ]
--- mshop/settings.py
+++ mshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%2lc=q*so()la-l7_y&vwu9qjcq2%ty2i-voeqwslalicn=du&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
