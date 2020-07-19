4f7d1085bca9dcb971ff827812cda43f80a96e71
Moonsur <monsur.cs.it@gmail.com>
2020-07-19 19:25:06 +0600

    Create admin supper user Changed database to mshop.sqlite3, database migrate create templates/movie/index.html

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Genre, Movie
 
-# Register your models here.
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('genre',)
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('name', 'rate', 'genre', 'reveiw')
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
